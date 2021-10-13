4ef3828ee00ba3fe5131cfa3027d58bccfdad7f6
Yū Kobayashi <kb10uy@kb10uy.org>
2021-10-13 14:24:56 +0900

    initial commit

---
+++ conf.d/quicker.fish
@@ -0,0 +1,20 @@
+bind \cf 'fast_travel'
+bind \cq 'quick_draw'
+
+function quicker_fast_travel
+  set target ( \
+    cat "$QUICKER_FAST_TRAVEL" | \
+    fzf -d ':' --with-nth 1 --height 30% --border --preview 'exa -la (eval {1})' \
+  )
+  commandline "cd $target"
+  commandline -f repaint
+end
+
+function quicker_quick_draw
+  set target ( \
+    cat "$QUICKER_QUICK_DRAW" | \
+    fzf -d ':' --with-nth 1 --height 30% --border --preview 'exa -la .' \
+  )
+  commandline $target
+  commandline -f repaint
+end
